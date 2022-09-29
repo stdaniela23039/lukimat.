5b6cba9e769486f620ad31f6e3f569201e42539f
yency rojas <ydrojaspo@iemsantateresita.edu.co>
2022-09-29 12:37:15 -0500

    pantalla perfil1

---
+++ app/src/main/java/com/stasoft/lukimat/PerfilActivity.kt
@@ -0,0 +1,11 @@
+package com.stasoft.lukimat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
