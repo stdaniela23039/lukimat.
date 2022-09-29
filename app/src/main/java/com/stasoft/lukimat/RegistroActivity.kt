283485668616322a5fa42bf9351ca6964e23dfc7
DanixaLopez <adcasas@iemsantateresita.edu.co>
2022-09-29 11:25:59 -0500

    Pantalla Registro

---
+++ app/src/main/java/com/stasoft/lukimat/RegistroActivity.kt
@@ -0,0 +1,11 @@
+package com.stasoft.lukimat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
